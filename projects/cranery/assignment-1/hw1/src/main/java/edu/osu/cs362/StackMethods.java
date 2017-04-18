432cd032d63449c0126ff654eac694e86e482c27
narxy <cranery@oregonstate.edu>
2017-04-17 21:47:11 -0700

    yup

---
+++ projects/cranery/assignment-1/hw1/src/main/java/edu/osu/cs362/StackMethods.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class StackMethods {
+    private int top;
+    int size;
+    int[] stack ;
+
+    public StackMethods(int arraySize){
+        size=arraySize;
+        stack= new int[size];
+        top=-1;
+    }
+
+    public void push(int value){
+        if(top>size-1){
+            System.out.println("Stack is full, can't push a value");
+        }
+        else{
+
+            top=top+1;
+            stack[top]=value;
+        }
+    }
+
+    public void pop(){
+        if(!isEmpty())
+            top=top-1;
+        else{
+            System.out.println("Can't pop...stack is empty");
+        }
+    }
+
+    public boolean isEmpty(){
+        return top==-1;
+    }
+
+    public void display(){
+
+        for(int i=0;i>=top;i++){
+            System.out.print(stack[i]+ " ");
+        }
+        System.out.println();
+    }
+}
